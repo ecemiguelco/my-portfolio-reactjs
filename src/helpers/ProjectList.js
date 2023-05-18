b6fc310c4082844f471a62fbdbbd57b561dfc494
Miguel Lorenzo Co <ece.miguel.co@gmail.com>
2023-05-18 14:39:46 +0800

    fix: Added index.css file

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -9,20 +9,24 @@ export const projectList = [
     name: "BudgetMeSoftly",
     image: Proj1,
     skills: "JavaScript,CSS,React.js,Chart.js,Node.js,Express.js,MonogDB",
+    github: "https://github.com/ecemiguelco/p5-budgetly",
   },
   {
     name: "Codegram",
     image: Proj2,
     skills: "Javascript,CSS,React.js,Node.js,Express.js,MongoDB",
+    github: "https://github.com/ecemiguelco/codegram",
   },
   {
     name: "MiggyBird",
     image: Proj3,
     skills: "Javascript,CSS,HTML",
+    github: "https://github.com/ecemiguelco/MiggyBird",
   },
   {
     name: "Digital Clock",
     image: Proj4,
     skills: "Javascript,CSS,HTML",
+    github: "https://github.com/ecemiguelco/digital-clock",
   },
 ];
